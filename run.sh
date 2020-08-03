164bd6e2d571c734449d42ec653935ec3f4b2477
Amir Samary <amirsamary@gmail.com>
2020-08-03 10:59:13 -0400

    Changing super server port to 1972

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ GIT_REPO_NAME=irisdemo-base-irisint-community
 IMAGE_NAME=intersystemsdc/$GIT_REPO_NAME:$TAG
 
 docker run --rm -it --init  \
-    -p 51773:51773 -p 52773:52773 \
+    -p 1972:1972 -p 52773:52773 \
     $IMAGE_NAME
